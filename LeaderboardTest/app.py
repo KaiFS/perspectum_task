565f0e029b2b6993e20775e7f679a867cf57de48
Ki Pickering <kai.fs1996@gmail.com>
2025-05-21 17:02:03 +0100

    Filter user data correctly, display filtered user data, very basic

--- LeaderboardTest/app.py
+++ LeaderboardTest/app.py
@@ -1,14 +1,24 @@
 from flask import Flask
 import json
-from leaderboard_logic import load_scores_data, SCORES_FILE_PATH, process_users_for_leaderboard
+from leaderboard_logic import load_scores_data, process_users_for_leaderboard
 
 app = Flask(__name__)
 
 @app.route("/")
-def show_all_data():
-    all_scores_data = load_scores_data(SCORES_FILE_PATH)
-    display_data = json.dumps(all_scores_data)
-    return display_data
+def show_processed_leaderboard_data():
+    all_raw_scores_data = load_scores_data()
+    processed_user_list = process_users_for_leaderboard(all_raw_scores_data)
+
+    data_to_display = "<h1>User scores</h1>"
+    if processed_user_list:
+        data_to_display += "<ul>"
+        for user_data_item in processed_user_list:
+            name = user_data_item.get('name')
+            score = user_data_item.get('leaderboard_score')
+            data_to_display += f"<li>{name}: {score}</li>" # Add each user as a list item
+        data_to_display += "</ul>"
+    #data_to_display = json.dumps(processed_user_list)
+    return data_to_display
 
 if __name__ == "__main__":
     app.run(debug=True)
