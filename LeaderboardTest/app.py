b861bd980ae24cafbe7c6ad63d84b91627873d88
Ki Pickering <kai.fs1996@gmail.com>
2025-06-03 11:51:20 +0100

    revert back to just using logic to refresh on request

--- LeaderboardTest/app.py
+++ LeaderboardTest/app.py
@@ -6,31 +6,46 @@ logging.basicConfig(level=logging.INFO, format="%(levelname)s - %(message)s")
 
 app = Flask(__name__)
 
-class DataManager:
-    """Manages the dataset for this project"""
-    def __init__(self):
-        self._leaderboard_data = self._initialise_data() # Load and store on instantiation instead
+# class DataManager:
+#     """Manages the dataset for this project"""
+#     def __init__(self):
+#         self._leaderboard_data = self._initialise_data() # Load and store on instantiation instead
         
-    def _initialise_data(self):
-        raw_data = load_scores_data()
-        if raw_data:
-            processed_data = process_final_users_for_leaderboard(raw_data)
-            if not processed_data:
-                logging.warning("Leaderboard data was processed, but no eligible users were found")
-            else:
-                logging.info("Found and loaded the processed leaderboard data successfully")
-            return processed_data
-        logging.error("Failed to load or process the raw data for leaderboard")
-        return []
+#     def _initialise_data(self):
+#         raw_data = load_scores_data()
+#         if raw_data:
+#             processed_data = process_final_users_for_leaderboard(raw_data)
+#             if not processed_data:
+#                 logging.warning("Leaderboard data was processed, but no eligible users were found")
+#             else:
+#                 logging.info("Found and loaded the processed leaderboard data successfully")
+#             return processed_data
+#         logging.error("Failed to load or process the raw data for leaderboard")
+#         return []
                 
-    def get_leaderboard(self):
-        return self._leaderboard_data
+#     def get_leaderboard(self):
+#         return self._leaderboard_data
                 
-data_manager = DataManager()
+# data_manager = DataManager()
+
+# @app.route("/")
+# def display_leaderboard():
+#     users_to_display = data_manager.get_leaderboard()
+#     return render_template("leaderboard.html", users_to_display=users_to_display)
 
 @app.route("/")
 def display_leaderboard():
-    users_to_display = data_manager.get_leaderboard()
+    users_to_display = []
+    all_raw_scores_data = load_scores_data()
+    if all_raw_scores_data is not None:
+        processed_leaderboard_data = process_final_users_for_leaderboard(all_raw_scores_data)
+        if not processed_leaderboard_data:
+            logging.warning("No eligible users found")
+        else:
+            logging.info("Leaderboard data found successfully")
+            users_to_display = processed_leaderboard_data
+    else:
+        logging.error("Failed to load raw scores data, leaderboard will be empty")
     return render_template("leaderboard.html", users_to_display=users_to_display)
 
 if __name__ == "__main__":
